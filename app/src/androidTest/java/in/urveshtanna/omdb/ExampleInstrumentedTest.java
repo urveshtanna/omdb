f91d47b33a075f5adf527353ba7850cd93e19753
Urvesh Tanna <ubtanna@gmail.com>
2017-06-30 16:47:06 +0530

    updated test case

--- app/src/androidTest/java/in/urveshtanna/omdb/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/in/urveshtanna/omdb/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("xyz", appContext.getPackageName());
+        assertEquals("in.urveshtanna.omdb", appContext.getPackageName());
     }
 }
