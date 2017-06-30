724f82054405491f9ab78ee29d5c591ee3d310a9
Urvesh Tanna <ubtanna@gmail.com>
2017-06-30 17:06:37 +0530

    added espresso testing

--- app/src/androidTest/java/in/urveshtanna/omdb/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/in/urveshtanna/omdb/ExampleInstrumentedTest.java
@@ -2,12 +2,20 @@ package in.urveshtanna.omdb;
 
 import android.content.Context;
 import android.support.test.InstrumentationRegistry;
+import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import static org.junit.Assert.*;
+import in.urveshtanna.omdb.activity.HomePageActivity;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.junit.Assert.assertEquals;
 
 /**
  * Instrumentation test, which will execute on an Android device.
@@ -16,11 +24,19 @@ import static org.junit.Assert.*;
  */
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
+
+    @Rule
+    public ActivityTestRule<HomePageActivity> mActivityTestRule = new ActivityTestRule<>(HomePageActivity.class);
+
     @Test
     public void useAppContext() throws Exception {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
-
         assertEquals("in.urveshtanna.omdb", appContext.getPackageName());
+
+
+        onView(withId(R.id.hint_to_search)).check(matches(withText(R.string.search_for_a_movie_or_tv_series)));
+
+
     }
 }
